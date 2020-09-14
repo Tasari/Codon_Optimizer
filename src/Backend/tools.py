3e7173bd9a4268f624d4b2d8bf19142556e8ef3f
Tasari <Tasari@wp.pl>
2020-09-14 14:36:04 +0200

    Tools docstrings

--- src/Backend/tools.py
+++ src/Backend/tools.py
@@ -3,28 +3,33 @@ import re
 
 
 def rewrite_to_rna(sequence):
+    """Returns sequence rewritten to RNA and in upper letters."""
     sequence = sequence.replace("T", "U").replace("\n", "").upper()
     return sequence
 
 
 def rewrite_sequence_to_protein(sequence):
+    """Returns sequence of aminoacids symbolizing protein from sequence."""
     return rewrite_codons_to_protein(rewrite_sequence_to_codons(sequence))
 
 
 def rewrite_sequence_to_codons(sequence):
+    """Returns list of codons created from the sequence."""
     codons = []
     base = 0
-    for i in range(int(len(sequence) / 3)):
+    for _ in range(int(len(sequence) / 3)):
         codons.append(sequence[base] + sequence[base + 1] + sequence[base + 2])
         base += 3
     return codons
 
 
 def rewrite_sequence_to_aminoacids(sequence):
+    """Returns list of aminoacids created from sequence."""
     return rewrite_codons_to_aminoacids(rewrite_sequence_to_codons(sequence))
 
 
 def rewrite_codons_to_sequence(codons):
+    """Returns sequence created from list of codons."""
     sequence = ""
     for codon in codons:
         sequence += codon
@@ -32,6 +37,7 @@ def rewrite_codons_to_sequence(codons):
 
 
 def rewrite_codons_to_protein(codons):
+    """Returns protein sequence created from list of codons"""
     protein = ""
     for codon in codons:
         protein += codon_to_aminoacid[codon]
@@ -39,6 +45,7 @@ def rewrite_codons_to_protein(codons):
 
 
 def rewrite_codons_to_aminoacids(codons):
+    """Creates list of ordered aminoacids from codons list"""
     aminoacids = []
     for codon in codons:
         aminoacids.append(codon_to_aminoacid[codon])
@@ -46,6 +53,13 @@ def rewrite_codons_to_aminoacids(codons):
 
 
 def get_most_frequent_codons(formatted_codons):
+    """Returns the dict of most frequent codons coding each aminoacid.
+    
+    Creates dictionary where to each aminoacid, being key, the codon
+    coding this aminoacid and with highest frequency is connected
+    based on list of Codon objects.
+    """
+
     optimal_codon = {}
     for codon in formatted_codons:
         if codon.aminoacid not in optimal_codon.keys():
@@ -56,14 +70,15 @@ def get_most_frequent_codons(formatted_codons):
 
 
 def find_sequence_in_gene(sequence, gene):
-    found_indexes = [
+    """Returns list of starts of occurances of sequence in gene"""
+    return [
         found.start()
         for found in re.finditer(re.compile(rewrite_to_rna(sequence)), gene)
     ]
-    return found_indexes
 
 
 def create_codon_bias_supersequence(formatted_codons):
+    """Creates string containing each codon times this codon's amount"""
     superstring = ""
     for codon in formatted_codons:
         superstring += codon.bases * codon.amount
